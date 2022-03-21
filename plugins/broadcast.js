6e8a129026b6c7ed8ce061bcf66a3914d1745791
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-22 01:58:52 +0530

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, text }) => {
   let chats = conn.chats.all().filter(v => v.jid.endsWith('.net')).map(v => v.jid).filter(v => !v.startsWith(owner[0]))
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `Mengirim pesan broadcast ke ${chats.length} chat\nEstimasi selesai ${chats.length * 1.5} detik`, m)
+  conn.reply(m.chat, ``Send a broadcast message to ${chats.length} chat\nEstimated complete ${chats.length * 1.5} second`, m)
   for (let id of chats) {
     await conn.delay(1500)
     await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '「 *Naruto Broadcast* 」\n\n' + teks + '\n\n© Naruto By Rafi'), true).catch(_ => _)
   }
-  m.reply('*Broadcast Selesai*')
+  m.reply('*Broadcast Completed*')
 }
 handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
