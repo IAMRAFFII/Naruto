e2acb8c844796912495b3166d989b0e7aa75b56c
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-22 00:38:34 +0530

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -11,14 +11,14 @@ handler.before = function (m, { isOwner, isBotAdmin }) {
 
     if (!chat.antiBadword && !chat.isBanned && isBadword) {
         user.warning += 1
-        this.send2Button(m.chat, `*Badword terdeteksi!*
+        this.send2Button(m.chat, `*Bad words detected!*
 Warning: ${user.warning} / 5
-Jika warning mencapai 5 kamu akan dibanned
+If the warning reaches 5 you will be banned
 
-ketik *#on antibadword* untuk menyalakan antibadword
-ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
+type *#on antibadword* to turn on antibadword 
+type *#sorry* to reduce warning
 
-“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, '© stikerin', 'Nyalakan Antibadword', ',1 antibadword', 'Astaghfirullah', ',maaf', m)
+“Whoever believes in God and the Last Day, let him say good or be silent” (HR. al-Bukhari dan Muslim).`, '© Naruto By Rafi Ser', 'Turn on Antibadword', ',1 antibadword', 'Astaghfirullah', ',maaf', m)
         if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
