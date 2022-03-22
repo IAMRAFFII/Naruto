5082ef03688e05c1c1d8c6ffb39cab8bfc3c3752
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-22 16:45:12 +0530

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -10,8 +10,8 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
   if (!url) throw '404 Not Found'
   conn.sendFile(m.chat, url, 'gimage', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
-handler.help = ['gimage <pencarian>', 'image <pencarian>']
+handler.help = ['img <Search>', 'image <pencarian>']
 handler.tags = ['internet']
-handler.command = /^(g?image)$/i
+handler.command = /^(i?mg)$/i
 
 module.exports = handler 
