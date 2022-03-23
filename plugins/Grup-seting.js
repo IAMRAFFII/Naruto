0d7e61724e0711654b2e2cf0dfe687948ad20c6c
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-23 13:44:11 +0530

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -12,18 +12,18 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	}[(args[0] || '')]
 	if (isClose === undefined) {
 		await conn.send2Button(m.chat, `
-Pengunaan:
-${usedPrefix + command} <buka/tutup>
+Use:
+${usedPrefix + command} <open/close>
 
-Contoh:
-${usedPrefix + command} tutup
-${usedPrefix + command} buka
-	`.trim(), '© stikerin', 'Buka', ',grup 1', 'Tutup', ',grup 0', m)
+Example:
+${usedPrefix + command} close
+${usedPrefix + command} open
+	`.trim(), '© Naruto By Rafi Ser', 'open✨', ',grup 1', 'Close🚫', ',grup 0', m)
 		throw 0
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
 }
-handler.help = ['grup <buka/tutup>']
+handler.help = ['grup <open/close>']
 handler.tags = ['group']
 handler.command = /^(gro?up)$/i
 
