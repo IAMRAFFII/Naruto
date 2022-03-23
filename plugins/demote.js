8a68b543e59302f55359b1a20124605aa8517500
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-23 13:32:35 +0530

    Update demote.js

--- plugins/demote.js
+++ plugins/demote.js
@@ -7,7 +7,6 @@ let handler = async (m, { conn, participants }) => {
   for (let user of users) await conn.groupDemoteAdmin(m.chat, [user]).catch(console.log)
 }
 handler.help = ['demote', 'member', '↓'].map(v => v + ' @user')
-handler.tags = ['admin']
 
 handler.command = /^(demote|member|↓)$/i
 
