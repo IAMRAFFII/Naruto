9c8b7b65e2b7d9df12dce9e52f86838ef1ffbf4f
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-23 16:42:00 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -377,7 +377,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'Bot Make By Rafi Ser🍭✨', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'Bot Make By Rafi Ser🍭✨', 'Bot Owner💫', `${_p}owner`, 'Donation', `${_p}donation`, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
