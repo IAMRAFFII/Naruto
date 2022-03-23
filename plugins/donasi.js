b9a410ca9937b98d0f2e01f897ebaeed44de08ca
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2022-03-23 16:39:44 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'Donation', `
 ┌「 *Donation • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ 
+├ 
 └────
 `.trim(), '© Naruto By Rafi Ser', 'Donation', '.donation', m)
 handler.help = ['donation']
